dabc3cbab5f75cdf35f369540aa875a5a10e34dd
AVRSANAND <avrsanand2003@gmail.com>
2024-06-13 12:56:21 +0530

    Add Tables

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from pathlib import Path
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
 
-    # Predefined filepath
+    # Filepaths without pathlib
 
     # Doc_Name = filepath.strip(f"'Invoices\\'")
     # print(Doc_Name)
@@ -19,22 +17,40 @@ for filepath in filepaths:
     # Date = Doc_Name[6:15]
     # print(Date)
 
-    # for index,item in df.iterrows():
-    #     print(item["product_id"])
-    #     print(item["product_name"])
-    #     print(item["price_per_unit"])
-    #     print(item["total_price"])
-
     Doc_Name = Path(filepath).stem
     Invoice_No, Date = Doc_Name.split("-")
 
     pdf = FPDF(orientation="P",unit="mm",format="A4")
     pdf.add_page()
+
     pdf.set_font(family="Times",size=16,style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice no. {Invoice_No} ",ln=1 )
+
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date {Date} ", ln=1)
-    # for index, item in df.iterrows():
-    #     pdf.set_font(family="Times", size=12)
-    #     pdf.multi_cell(w=200, h=8, txt=f"{item['product_id']} | {item['product_name']} | {item['price_per_unit']} | {item['total_price']}")
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Header
+    headers = list(df.columns)
+    headers = [item.replace("_", " ").title() for item in headers]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80,80,80)
+
+    pdf.cell(w=30, h=8, txt=headers[0], border=1)
+    pdf.cell(w=70, h=8, txt=headers[1], border=1)
+    pdf.cell(w=35, h=8, txt=headers[2], border=1)
+    pdf.cell(w=30, h=8, txt=headers[3], border=1)
+    pdf.cell(w=30, h=8, txt=headers[4], border=1, ln=1)
+
+    # Values
+    for index, item in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=f"{item['product_id']}",border=1)
+        pdf.cell(w=70, h=8, txt=f"{item['product_name']}",border=1)
+        pdf.cell(w=35, h=8, txt=f"{item['amount_purchased']}",border=1)
+        pdf.cell(w=30, h=8, txt=f"{item['price_per_unit']}",border=1)
+        pdf.cell(w=30, h=8, txt=f"{item['total_price']}",border=1, ln=1)
+
     pdf.output(f"PDFs/{Doc_Name}.pdf")
