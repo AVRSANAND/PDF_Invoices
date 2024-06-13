7131bc61b0f84e3bfe6f6c0cc3dd2dc0b293e310
AVRSANAND <avrsanand2003@gmail.com>
2024-06-13 12:24:08 +0530

    Add invoice pdfs

--- main.py
+++ main.py
@@ -19,15 +19,22 @@ for filepath in filepaths:
     # Date = Doc_Name[6:15]
     # print(Date)
 
+    # for index,item in df.iterrows():
+    #     print(item["product_id"])
+    #     print(item["product_name"])
+    #     print(item["price_per_unit"])
+    #     print(item["total_price"])
+
     Doc_Name = Path(filepath).stem
-    Invoice_No = Doc_Name.split("-")[0]
-    Date = Doc_Name.split("-")[1]
+    Invoice_No, Date = Doc_Name.split("-")
 
     pdf = FPDF(orientation="P",unit="mm",format="A4")
     pdf.add_page()
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {Invoice_No} ",ln=1 )
+    pdf.cell(w=50, h=8, txt=f"Invoice no. {Invoice_No} ",ln=1 )
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date {Date} ", ln=1)
-
+    # for index, item in df.iterrows():
+    #     pdf.set_font(family="Times", size=12)
+    #     pdf.multi_cell(w=200, h=8, txt=f"{item['product_id']} | {item['product_name']} | {item['price_per_unit']} | {item['total_price']}")
     pdf.output(f"PDFs/{Doc_Name}.pdf")
